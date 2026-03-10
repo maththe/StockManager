40a4d156f8564e3a7932137f63b8ef7dc959ea07
Matheus Bernardi <matheusantoniobernardi@gmail.com>
2026-03-10 18:50:03 -0300

    arrumando projeto

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,4 +1,3 @@
-// src/prisma/prisma.service.ts
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client/extension';
 
@@ -6,7 +5,6 @@ import { PrismaClient } from '@prisma/client/extension';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    // Connect to the database when the module is initialized
     await this.$connect();
   }
 }
