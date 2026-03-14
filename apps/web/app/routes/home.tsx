cfffed4d99bde528decaa9c3b9b772b3c75910bb
Seu Nome Aqui <seu.email.trabalho@exemplo.com>
2026-03-14 17:26:54 -0300

    configarão concluida

--- apps/web/app/routes/home.tsx
+++ apps/web/app/routes/home.tsx
@@ -1,16 +1,10 @@
-import type { Route } from "./+types/home";
+import { Route, Routes } from "react-router";
 import Login from "./users/components/login";
 
-
-
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
-
 export default function Home() {
-  return <Login />;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
+  );
 }
